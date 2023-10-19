303ba018f7c459b424ddbbc349b242544e3c4ff1
Krisha012 <Krishalukhi2312@gmail.com>
2023-10-19 10:16:53 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <h1 className="font-semibold text-4xl text-center"><span className="text-brightGreen ">e</span>Study</h1>
+        </div>
+      </div>
+      <div className=" text-center my-5 ">
+        <p>
+          @copyright developed by
+          <span className="text-brightGreen px-2">champion programmers</span>|
+          All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
